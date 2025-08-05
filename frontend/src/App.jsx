0bb76f18ba578dc0c9929968ccf758dd30a8a9f4
Zzc0907 <3290456877@qq.com>
2025-08-05 23:42:55 +0800

    完善了登录界面，补充了注册界面的内容，同时添加了背景图片

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,25 +11,36 @@ import AllBlindBox from "./pages/AllBlindBox"
 import MyBlindBox from "./pages/MyBlindBox"
 import BlindBoxDetails from "./pages/BlindBoxDetails"
 import FindBlindBox from "./pages/FindBlindBox"
+
 function App() {
     return (
-        <BrowserRouter>
-            <Routes >
-                <Route path="*" element={<Login />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/SelfMessage/:id" element={<SelfMessage />} />
-                <Route path="/CreateBlindBox" element={<CreateBlindBox />} />
-                <Route path="/GetBlindBox" element={<GetBlindBox />} />
-                <Route path="/BlindBoxOrder/:id" element={<BlindBoxOrder />} />
-                <Route path="/AllBlindBox" element={<AllBlindBox />} />
-                <Route path="/MyBlindBox/:id" element={<MyBlindBox />} />
-                <Route path="/BlindBoxDetails/:id" element={<BlindBoxDetails />} />
-                <Route path="/FindBlindBox" element={<FindBlindBox />} />
-            </Routes>
-        </BrowserRouter>
+        <div className="min-h-screen w-full relative">
+            {/* 背景图层：保留原始样式，无透明无模糊 */}
+            <div
+                className="fixed inset-0 bg-cover bg-center z-0"
+                style={{ backgroundImage: "url('/背景图.jpg')" }}
+            ></div>
+
+            {/* 内容层 */}
+            <div className="relative z-10 min-h-screen">
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="*" element={<Login />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                        <Route path="/SelfMessage/:id" element={<SelfMessage />} />
+                        <Route path="/CreateBlindBox" element={<CreateBlindBox />} />
+                        <Route path="/GetBlindBox" element={<GetBlindBox />} />
+                        <Route path="/BlindBoxOrder/:id" element={<BlindBoxOrder />} />
+                        <Route path="/AllBlindBox" element={<AllBlindBox />} />
+                        <Route path="/MyBlindBox/:id" element={<MyBlindBox />} />
+                        <Route path="/BlindBoxDetails/:id" element={<BlindBoxDetails />} />
+                        <Route path="/FindBlindBox" element={<FindBlindBox />} />
+                    </Routes>
+                </BrowserRouter>
+            </div>
+        </div>
     )
 }
-
 
 export default App
